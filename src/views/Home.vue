9fa8e5e9370d4c3c9620ce1bb3467a25ddfcfa81
Kevin Heijboer <kevin.heijboer@gmail.com>
2021-03-14 14:00:19 +0100

    changed home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,11 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="het werkt ik ben zo blij :D 😀" />
+    <h1>Quacker 🦆</h1>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld,
-  },
 };
 </script>
