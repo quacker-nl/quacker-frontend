a912763f1de1ac5fcffa5f2427e74c69278f7fce
Kevin Heijboer <kevin.heijboer@gmail.com>
2021-03-18 02:23:26 +0100

    +ADD: basic quack functionality

---
+++ src/services/quack.service.js
@@ -0,0 +1,19 @@
+import axios from '@/axios';
+
+const ENDPOINT = '/quacks';
+
+class QuackService {
+  async getQuacks() {
+    return await axios.get(ENDPOINT);
+  }
+
+  async getQuack(quackId) {
+    return await axios.get(`${ENDPOINT}/${quackId}`);
+  }
+
+  async addQuack(quack) {
+    return await axios.post(ENDPOINT, quack);
+  }
+}
+
+export default new QuackService();
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,65 @@
 <template>
   <div class="home">
     <h1>🦆Welcome to Quacker🦆</h1>
-    <p>(met cloudflare)</p>
-    <p>pogchamp</p>
-    <form>
-      <input type="text" placeholder="username" />
-      <input type="password" placeholder="password" />
-    </form>
+    <div class="whats-happening">
+      <h2>What's happening?</h2>
+      <div class="quack">
+        <textarea id="submit-quack-area" v-model="quackMessage" />
+        <button @click="addQuack" class="submit-button">Quack</button>
+      </div>
+    </div>
+    <div class="quacks-timeline">
+      <h2>Timeline</h2>
+      <div class="quack" v-for="quack in quacks" :key="quack.id">
+        <p class="quack-message">{{ quack.message }}</p>
+        <p class="quack-date">{{ quack.createdOn | formatDate }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import QuackService from '@/services/quack.service.js';
+import moment from 'moment';
+
 export default {
   name: 'Home',
+  data() {
+    return {
+      quacks: [],
+      quackMessage: '',
+    };
+  },
+  methods: {
+    addQuack() {
+      let quack = {
+        message: this.quackMessage,
+      };
+      QuackService.addQuack(quack).then((response) => {
+        console.log(response.data);
+        this.quacks.push(response.data);
+        this.quacks.sort(function(a, b) {
+          return new Date(b.createdOn) - new Date(a.createdOn);
+        });
+      });
+    },
+  },
+  mounted() {
+    QuackService.getQuacks().then((response) => {
+      this.quacks = response.data;
+      this.quacks.sort(function(a, b) {
+        return new Date(b.createdOn) - new Date(a.createdOn);
+      });
+    });
+  },
+  filters: {
+    formatDate: function(date) {
+      if (date) {
+        return moment(String(date)).fromNow();
+      }
+    },
+  },
 };
 </script>
+
+<style scoped src="@/styles/home.css"></style>
