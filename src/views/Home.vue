1bef9df6ce8be76d3f253244a5689f8197be631c
Kevin Heijboer <kevin.heijboer@gmail.com>
2021-04-06 10:01:55 +0200

    +ADD: navbar in component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,58 +1,7 @@
 <template>
   <div class="home">
     <div class="row">
-      <div class="col-3 nav-col">
-        <div class="navbar">
-          <div class="logo-container" @click="goTo('')">
-            <img class="logo" src="@/assets/logo.png" alt="logo" />
-            <h1>Quacker</h1>
-          </div>
-
-          <ul>
-            <li>
-              <md-button
-                @click="goTo('')"
-                class="nav-button md-dense md-raised md-primar"
-              >
-                <h2
-                  :class="{
-                    active: this.$route.name == 'Home',
-                  }"
-                >
-                  <span class="material-icons">home</span>Home
-                </h2></md-button
-              >
-            </li>
-            <li>
-              <md-button
-                @click="goTo('mentions')"
-                class="nav-button md-dense md-raised md-primary"
-              >
-                <h2
-                  :class="{
-                    active: this.$route.name == 'Mentions',
-                  }"
-                >
-                  <span class="material-icons">notifications</span>Mentions
-                </h2></md-button
-              >
-            </li>
-            <li>
-              <md-button
-                @click="goTo('profile')"
-                class="nav-button md-dense md-raised md-primary"
-                ><h2
-                  :class="{
-                    active: this.$route.name == 'Profile',
-                  }"
-                >
-                  <span class="material-icons">person</span>Profile
-                </h2></md-button
-              >
-            </li>
-          </ul>
-        </div>
-      </div>
+      <div class="col-3 nav-col"><Navbar /></div>
       <div class="col-5 main-col"></div>
       <div class="col-4 side-col"></div>
     </div>
@@ -62,6 +11,7 @@
 <script>
 import QuackService from '@/services/quack.service.js';
 import moment from 'moment';
+import Navbar from '@/components/Navbar.vue';
 
 export default {
   name: 'Home',
@@ -70,11 +20,10 @@ export default {
       quacks: [],
     };
   },
-  methods: {
-    goTo(path) {
-      this.$router.push(path).catch(() => {});
-    },
+  components: {
+    Navbar,
   },
+  methods: {},
   mounted() {},
   filters: {
     formatDate: function(date) {
