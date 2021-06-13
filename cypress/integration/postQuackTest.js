5cdbf1f0059e182ad2d5a7c504f33760e5f73168
Ecksedee <kevin.heijboer@gmail.com>
2021-06-13 02:22:51 +0200

    ~FIX: e2e

--- cypress/integration/postQuackTest.js
+++ cypress/integration/postQuackTest.js
@@ -9,7 +9,7 @@ var admin = {
 
 describe('Posting quacks', () => {
   it('Post quack should show quack in timeline', () => {
-    cy.login(admin.username, admin.password);
+    cy.visit(baseURL + 'login');
 
     cy.get('textarea[id=quack-textarea]').type(faker.random.words());
     cy.get('button[id=quack-button]').click({ force: true });
